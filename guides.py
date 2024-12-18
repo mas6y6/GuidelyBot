aa045c8e3d663f641aea5382b4d0efde4f260768
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2024-12-17 19:50:55 -0600

    Changes

--- guides.py
+++ guides.py
@@ -19,3 +19,39 @@ class InterductionGuides(discord.ui.View):
     @discord.ui.button(label="Maintaining the reactor")
     async def b4(self, interaction: discord.Interaction, button: discord.Button):
         pass
+
+class GameplayGuides(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+    @discord.ui.button(label="Exploring the facility")
+    async def b1(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+    @discord.ui.button(label="Hunger & Energy")
+    async def b2(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+    @discord.ui.button(label="Working as an employee")
+    async def b3(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+    @discord.ui.button(label="Working in maintenance")
+    async def b4(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+    @discord.ui.button(label="Working as a security")
+    async def b5(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+    @discord.ui.button(label="Working as a researcher")
+    async def b6(self, interaction: discord.Interaction, button: discord.Button):
+        pass
+    
+class OtherGuides(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+    @discord.ui.button(label="Events")
+    async def b1(self, interaction: discord.Interaction, button: discord.Button):
+        pass
--- main.py
+++ main.py
@@ -20,30 +20,37 @@ async def on_ready():
     await bot.change_presence(activity=game)
     print(len(cmds),"Commands synced !")
 
-
-
 class GuideSelectionView(discord.ui.View):
     def __init__(self):
         super().__init__()
         
     @discord.ui.button(label="Introduction Guides")
     async def b1(self, interaction: discord.Interaction, button: discord.Button):
-        pass
+        embed = discord.Embed(color=discord.Color.purple(),description=f"""These guides are to introduce you to ICSCS and helping to maintain the reactor!
+
+**Select one the the catagories below to select your guide!**""",title="Introduction Guides")
+        await interaction.response.edit_message(view=guides.InterductionGuides(),embed=embed)
     
     @discord.ui.button(label="Gameplay Guides")
     async def b2(self, interaction: discord.Interaction, button: discord.Button):
-        pass
+        embed = discord.Embed(color=discord.Color.purple(),description=f"""These guides are to introduce you to the gameplay mechanics of ICSCS
+
+**Select one the the catagories below to select your guide!**""",title="Gameplay Guides")
+        await interaction.response.edit_message(view=guides.GameplayGuides(),embed=embed)
     
     @discord.ui.button(label="Other Guides")
     async def b3(self, interaction: discord.Interaction, button: discord.Button):
-        pass
+        embed = discord.Embed(color=discord.Color.purple(),description=f"""These guides extra you dont really need them but here they are!
+
+**Select one the the catagories below to select your guide!**""",title="Other Guides")
+        await interaction.response.edit_message(view=guides.OtherGuides(),embed=embed)
 
 @bot.tree.command(name="guide",description="Opens a guide from ICSCS")
 async def guide(interaction: discord.Interaction):
     embed = discord.Embed(color=discord.Color.purple(),description=f"""Hi {interaction.user.mention}! I am guidley!
 I have ported my guides from ICSCS to discord so you can still use the guides even when you are not in the game!
 
-**Select one the the catagories below to select your guide!**""")
+**Select one the the catagories below to select your guide!**""",title="Guides")
     
     view = GuideSelectionView()
     
