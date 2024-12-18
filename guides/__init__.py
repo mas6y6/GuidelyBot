32d8ad70874de607d4230b6ee7c72b10d00a40a1
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2024-12-18 17:52:24 -0600

    Push

--- guides/__init__.py
+++ guides/__init__.py
@@ -55,3 +55,13 @@ class OtherGuides(discord.ui.View):
     @discord.ui.button(label="Events")
     async def b1(self, interaction: discord.Interaction, button: discord.Button):
         pass
+    
+class GuideStartMenu(discord.ui.View):
+    def __init__(self,guideclass,guidename="Guide",description="Guide Description"):
+        self.guidename="Guide"
+        self.description=description
+        super().__init__()
+    
+    @discord.ui.button(label="Start")
+    async def b1(self,interaction: discord.Interaction, button: discord.Button):
+        
